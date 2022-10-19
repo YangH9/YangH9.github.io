cebf73a7b7ef8bdfc5028ae5ac465acb89a499c3
YangH9 <49155616+YangH9@users.noreply.github.com>
2022-10-19 15:53:49 +0800

    update

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+/* eslint-env node */
+require("@rushstack/eslint-patch/modern-module-resolution");
+
+module.exports = {
+  root: true,
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/eslint-config-prettier",
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "space-before-function-paren": 0, // 方法空格
+    "no-unused-vars": 1,
+    "vue/no-unused-components": "off",
+    "no-prototype-builtins": 0,
+  },
+};
