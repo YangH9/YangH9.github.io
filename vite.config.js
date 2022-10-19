27a41edecabb0e403261438dfd039d7a7ab8275d
Yang <49155616+YangH9@users.noreply.github.com>
2022-10-19 17:43:31 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -8,13 +8,13 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "@": fileURLToPath(new URL("./src", import.meta.url))
     },
   },
   build: {
     target: "modules",
     outDir: "dist",
     assetsDir: "assets",
-    minify: "terser",
+    minify: "terser"
   },
 });
