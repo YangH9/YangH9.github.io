7aa40cdaf4f3a5557977b63c217e991bef27bbd1
YangH9 <49155616+YangH9@users.noreply.github.com>
2022-10-19 17:43:31 +0800

    update

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,5 @@
 /* eslint-env node */
+
 require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
@@ -6,17 +7,32 @@ module.exports = {
   extends: [
     "plugin:vue/vue3-essential",
     "eslint:recommended",
-    "@vue/eslint-config-prettier",
+    "plugin:vue/recommended"
   ],
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
   parserOptions: {
     ecmaVersion: "latest",
+    sourceType: 'module',
   },
   rules: {
+    'no-var': 2,//禁用var 
+    semi: 2,//开启结尾分号
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     "space-before-function-paren": 0, // 方法空格
-    "no-unused-vars": 1,
+    "no-unused-vars": "off",
     "vue/no-unused-components": "off",
+    "vue/no-multiple-template-root": "off",
     "no-prototype-builtins": 0,
+    "no-setter-return": "off",
+    "no-cond-assign": "off",
+    "no-useless-escape": "off",
+    "no-unreachable": "off",
+    "no-empty": "off",
+    "no-undef": "off",
   },
 };
