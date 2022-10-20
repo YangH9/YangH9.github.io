50e5498f9c85ed6a35d4699660af080a87901379
YangH9 <49155616+YangH9@users.noreply.github.com>
2022-10-20 18:07:20 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,34 @@
 import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url))
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+  let config = {
+    plugins: [vue()],
+    root: env.VITE_ROOT_URL,
+    base: env.VITE_BASE_URL,
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url))
+      },
     },
-  },
-  server: {
-    host: '0.0.0.0',
-    port: 6060,
-    strictPort: true,
-  },
-  build: {
-    target: "modules",
-    outDir: "dist",
-    emptyOutDir: true,
-    assetsDir: "assets",
-    minify: "terser",
-  },
-});
+    server: {
+      host: '0.0.0.0',
+      port: 6060,
+      strictPort: true,
+    },
+    build: {
+      target: "modules",
+      outDir: env.VITE_OUTDIR_URL,
+      emptyOutDir: true,
+      assetsDir: "assets",
+      minify: "terser",
+    },
+  }
+
+  console.log(config)
+  return defineConfig(config)
+};
