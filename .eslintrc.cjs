fc58147df9339e98633d2ff0132e307a1aa18f1c
YangH9 <49155616+YangH9@users.noreply.github.com>
2022-10-20 16:39:59 +0800

    update

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,3 @@
-/* eslint-env node */
-
 require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
@@ -34,5 +32,6 @@ module.exports = {
     "no-unreachable": "off",
     "no-empty": "off",
     "no-undef": "off",
+    'vue/multi-word-component-names': 'off' // 关闭组件名称校验
   },
 };
