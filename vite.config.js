3e525dc07f4e6cfd20f6b79fcede05c21e3e4de4
Yang <49155616+YangH9@users.noreply.github.com>
2022-10-20 10:01:28 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: "./",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url))
@@ -15,6 +16,6 @@ export default defineConfig({
     target: "modules",
     outDir: "dist",
     assetsDir: "assets",
-    minify: "terser"
+    minify: "terser",
   },
 });
