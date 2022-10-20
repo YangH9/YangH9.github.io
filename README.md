3ecd8c0388e81401e5c9b7958732aeaeed7a536c
YangH9 <49155616+YangH9@users.noreply.github.com>
2022-10-20 10:54:23 +0800

    dist

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # minesweeper
 扫雷
 
-git subtree push --prefix dist origin dist
-
 This template should help get you started developing with Vue 3 in Vite.
 
 ## Recommended IDE Setup
--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  root: "./public",
   base: "/minesweeper/",
   resolve: {
     alias: {
@@ -14,7 +15,7 @@ export default defineConfig({
   },
   build: {
     target: "modules",
-    outDir: "dist",
+    outDir: "../dist",
     assetsDir: "assets",
     minify: "terser",
   },
