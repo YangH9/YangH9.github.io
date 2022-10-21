dbc3b835e5a970ba1d0ce286ee7e5e897455d9e0
YangHan <15238117739@163.com>
2022-10-21 23:01:46 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig, loadEnv } from "vite"
 import Components from "unplugin-vue-components/vite"
 import { AntDesignVueResolver } from "unplugin-vue-components/resolvers"
 import vue from "@vitejs/plugin-vue"
+import legacy from "@vitejs/plugin-legacy"
 
 export default ({ mode }) => {
   const env = loadEnv(mode, process.cwd())
@@ -10,8 +11,11 @@ export default ({ mode }) => {
     plugins: [
       vue(),
       Components({
-        resolvers: [AntDesignVueResolver()],
+        resolvers: [ AntDesignVueResolver() ],
       }),
+      legacy({
+        targets: [ "defaults", "not IE 11" ]
+      })
     ],
     root: env.VITE_ROOT_URL,
     base: env.VITE_BASE_URL,
