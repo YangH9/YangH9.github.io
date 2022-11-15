6262ecc1d1b2f2d51a095507a116ef17c9c3c8fb
YangH9 <49155616+YangH9@users.noreply.github.com>
2022-11-15 22:01:20 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { fileURLToPath, URL } from "node:url"
 import { defineConfig, loadEnv } from "vite"
 import Components from "unplugin-vue-components/vite"
+import viteCompression from "vite-plugin-compression"
 import { AntDesignVueResolver } from "unplugin-vue-components/resolvers"
 import vue from "@vitejs/plugin-vue"
 
@@ -11,6 +12,17 @@ export default ({ mode }) => {
       vue(),
       Components({
         resolvers: [AntDesignVueResolver()]
+      }),
+      viteCompression({
+        // 生成压缩包gz
+        verbose: true, // 是否在控制台输出压缩结果
+        filter: /\.(js|mjs|json|css|html)$/i, // 指定哪些资源不压缩
+        disable: false, // 是否禁用
+        threshold: 102400, // 体积大于 threshold 才会被压缩,单位 b
+        algorithm: "gzip", // 压缩算法,可选 [ 'gzip' , 'brotliCompress' ,'deflate' , 'deflateRaw']
+        ext: ".gz", // 生成的压缩包后缀
+        // compressionOptions: Object, // 对应的压缩算法的参数
+        deleteOriginFile: true // 压缩后是否删除源文件
       })
     ],
     root: env.VITE_ROOT_URL,
@@ -27,7 +39,7 @@ export default ({ mode }) => {
       strictPort: true
     },
     build: {
-      chunkSizeWarningLimit: 1500,
+      // chunkSizeWarningLimit: 1500,
       minify: "terser",
       target: "modules",
       outDir: env.VITE_OUTDIR_URL,
