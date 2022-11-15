0a4d440996433fc1096d1c3225c8ba6134fed157
YangH9 <49155616+YangH9@users.noreply.github.com>
2022-11-15 22:52:08 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -20,9 +20,7 @@ export default ({ mode }) => {
         disable: false, // 是否禁用
         threshold: 1024000, // 体积大于 threshold 才会被压缩,单位 b
         algorithm: "gzip", // 压缩算法,可选 [ 'gzip' , 'brotliCompress' ,'deflate' , 'deflateRaw']
-        ext: ".gz", // 生成的压缩包后缀
-        // compressionOptions: Object, // 对应的压缩算法的参数
-        deleteOriginFile: false // 压缩后是否删除源文件
+        ext: ".gz" // 生成的压缩包后缀
       })
     ],
     root: env.VITE_ROOT_URL,
