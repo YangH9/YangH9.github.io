222b6e486332a24b766e4be9b618263028b392ff
YangH9 <49155616+YangH9@users.noreply.github.com>
2022-12-02 11:17:38 +0800

    update

--- src/views/joy/tool/petpet/index.vue
+++ src/views/joy/tool/petpet/index.vue
@@ -6,13 +6,20 @@
         src="https://yangh9.github.io/petpet/"
         frameborder="0"
         width="100%"
-        height="100%"></iframe>
+        height="100%"
+        @load="routerLoading().hide()"></iframe>
     </a-layout-content>
   </div>
 </template>
 
 <script setup>
 import Breadcrumb from "@/components/Breadcrumb.vue"
+import { routerLoading } from "@/stores"
+import { onMounted } from "vue"
+
+onMounted(() => {
+  routerLoading().show()
+})
 </script>
 
 <style lang="less" scoped>
