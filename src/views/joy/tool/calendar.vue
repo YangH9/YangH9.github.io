bacf270126dffe1e0a78f5b807644d39ad4469d9
YangH9 <49155616+YangH9@users.noreply.github.com>
2023-12-01 17:51:47 +0800

    Update calendar.vue

--- src/views/joy/tool/calendar.vue
+++ src/views/joy/tool/calendar.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="container">
     <Breadcrumb overlayShow />
-    <a-card v-calcHeight="{ height: 18, dom: '.ant-card-body' }" title="日历" :hoverable="true">日历</a-card>
+    <a-card v-calcHeight="{ height: 18, dom: '.ant-card-body' }" title="日历" :hoverable="true">
+      <a-calendar></a-calendar>
+      <el-calendar></el-calendar>
+    </a-card>
   </div>
 </template>
 
