9f8f437cbd59f91e73334c4d60e8f83559bcd077
YangH9 <49155616+YangH9@users.noreply.github.com>
2023-12-01 17:51:52 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <template>
   <a-config-provider :locale="ant_zhCN">
-    <el-config-provider :locale="ele_zhCn">
-      <a-watermark :content="`${route.meta.title}-${title}-木易`" :zIndex="100" :gap="[200, 200]" :offset="[50, 50]">
-        <Loading />
-        <RouterView />
-      </a-watermark>
-    </el-config-provider>
+    <a-watermark :content="`${route.meta.title}-${title}-木易`" :zIndex="100" :gap="[200, 200]" :offset="[50, 50]">
+      <Loading />
+      <RouterView />
+    </a-watermark>
   </a-config-provider>
 </template>
 
@@ -14,7 +12,6 @@ import Loading from '@/components/Loading.vue'
 import { inject } from 'vue'
 import { useRoute } from 'vue-router'
 import ant_zhCN from 'ant-design-vue/es/locale/zh_CN'
-import ele_zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 
 const route = useRoute()
 const GetEnv = inject('GetEnv')
