67f148de2b6eb95a7bb2a593cb1dc0879d6655f1
YangH9 <49155616+YangH9@users.noreply.github.com>
2023-12-22 11:23:38 +0800

    [update]自动提交

--- docs/assets/js/chunk-46ded4fe.js
+++
@@ -1,1 +0,0 @@
-import{B as a}from"./chunk-3132445e.js";import{b as s,d as e,i as r,f as t,w as o,k as n}from"./index-ad99d1f8.js";const c={class:"container"},d={__name:"GrayHead",setup:d=>(d,i)=>{const l=s("a-card");return e(),r("div",c,[t(a,{overlayShow:""}),t(l,{title:"灰色头像生成器",class:"mb_2",hoverable:!0},{default:o((()=>[n(" 灰色头像生成器 ")])),_:1})])}};export{d as default};
