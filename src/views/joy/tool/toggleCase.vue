1a25225ac561054bc1904d00c1cff869b467e0e5
YangH9 <49155616+YangH9@users.noreply.github.com>
2024-02-09 23:02:45 +0800

    [update]自动提交

--- src/views/joy/tool/toggleCase.vue
+++ src/views/joy/tool/toggleCase.vue
@@ -9,6 +9,9 @@
 import Breadcrumb from '@/components/Breadcrumb.vue'
 
 // https://wannianli.tianqi.com/jisuanqi/daxie/
+const obj = { 0: '零', 1: '壹', 2: '贰', 3: '叁', 4: '肆', 5: '伍', 6: '陆', 7: '柒', 8: '捌', 9: '玖', 十: '拾' }
+const list = ['零', '壹', '贰', '叁', '肆', '伍', '陆', '柒', '捌', '玖', '拾']
+const list1 = ['零', '一', '二', '三', '四', '五', '六', '七', '八', '九', '十']
 </script>
 
 <style scoped lang="scss"></style>
