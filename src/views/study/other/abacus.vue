96c604f6e33226f0cdaf21a3ac50c26910a70ed4
YangH9 <49155616+YangH9@users.noreply.github.com>
2024-04-25 10:58:06 +0800

    Create abacus.vue

---
+++ src/views/study/other/abacus.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container">
+    <Breadcrumb overlayShow />
+    <a-card title="算盘" class="mb_2" :hoverable="true"> 算盘说明介绍 </a-card>
+  </div>
+</template>
+
+<script setup>
+import Breadcrumb from '@/components/Breadcrumb.vue'
+</script>
+
+<style scoped lang="scss"></style>
