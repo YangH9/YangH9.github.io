26a70fc63bd68b65d37c847c885a0c0f829115c5
YangH9 <49155616+YangH9@users.noreply.github.com>
2024-08-22 13:18:56 +0800

    [update]自动提交

--- src/views/study/other/time-wheel.vue
+++ src/views/study/other/time-wheel.vue
@@ -122,19 +122,26 @@ const transformItemStyleFilter = (index, key) => {
   const itemWidth = 420 / dateListCheck.value.length
   const dateIndex = dateListCheck.value.findIndex(item => item === key)
   return {
+    width: `${itemWidth}px`,
     transform: dateListCheck.value.includes(key)
       ? key !== 'hour'
-        ? `rotate(${index * degMap[key]}deg) translateX(${dateIndex * itemWidth + 60}px)`
-        : dateListCheck.value.includes('ampm') && index > 12
+        ? `translate(-50%, -50%) rotate(${index * degMap[key]}deg) translateX(${
+            dateIndex * itemWidth + itemWidth / 2 + 40
+          }px)`
+        : dateListCheck.value.includes('ampm') && index > 11
           ? 'scale(0)'
-          : `rotate(${index * degMap[key]}deg) translateX(${dateIndex * itemWidth + 60}px)`
+          : `translate(-50%, -50%) rotate(${index * degMap[key]}deg) translateX(${
+              dateIndex * itemWidth + itemWidth / 2 + 40
+            }px)`
       : 'scale(0)'
   }
 }
 
 const filterNum = num => {
   const chineseNumbers = ['', '一', '二', '三', '四', '五', '六', '七', '八', '九']
-  if (num < 10) {
+  if (!num) {
+    return '零'
+  } else if (num < 10) {
     return chineseNumbers[num]
   } else if (num >= 10 && num < 20) {
     return `十${chineseNumbers[num % 10]}`
@@ -157,9 +164,9 @@ const animation = () => {
   dateTime.year = `${year}年`
   dateTime.monthDeg = -(month - 1) * 30
   dateTime.dateDeg = -(date - 1) * (360 / dateLength.value)
-  dateTime.hourDeg = dateListCheck.value.includes('ampm') ? -((hour - 1) % 12) * 30 : -(hour - 1) * 15
-  dateTime.minuteDeg = -(minute - 1) * 6
-  dateTime.secondDeg = -(second - 1) * 6
+  dateTime.hourDeg = dateListCheck.value.includes('ampm') ? -(hour % 12) * 30 : -hour * 15
+  dateTime.minuteDeg = -minute * 6
+  dateTime.secondDeg = -second * 6
   dateTime.ampmDeg = -~~(hour / 12) * 180
 
   const transitionend = () => {
@@ -173,15 +180,15 @@ const animation = () => {
         dateTime.dateDeg = 360 - (date - 1) * (360 / dateLength.value)
       }
       if ((dateListCheck.value.includes('ampm') ? ((hour + 1) % 12) * 30 : (hour + 1) * 15) >= 360) {
-        dateTime.hourDeg = 360 - (dateListCheck.value.includes('ampm') ? -((hour - 1) % 12) * 30 : -(hour - 1) * 15)
+        dateTime.hourDeg = 360 - (dateListCheck.value.includes('ampm') ? -(hour % 12) * 30 : -hour * 15)
       }
       if ((minute + 1) * 6 >= 360) {
-        dateTime.minuteDeg = 360 - (minute - 1) * 6
+        dateTime.minuteDeg = 360 - minute * 6
       }
       if (~~((hour + 1) / 12) * 180 >= 360) {
         dateTime.ampmDeg = 360 - ~~(hour / 12) * 180
       }
-      dateTime.secondDeg = 360 - (second - 1) * 6
+      dateTime.secondDeg = 360 - second * 6
       setTimeout(
         () => {
           hasT.value = true
@@ -203,16 +210,15 @@ const init = () => {
   animation()
   dateTimeList.monthList = Array.from({ length: 12 }, (_, i) => `${filterNum(i + 1)}月`)
   dateTimeList.dateList = Array.from({ length: dateLength.value }, (_, i) => `${filterNum(i + 1)}日`)
-  dateTimeList.hourList = Array.from({ length: 24 }, (_, i) => `${filterNum(i + 1)}时`)
-  dateTimeList.minuteList = Array.from({ length: 60 }, (_, i) => `${filterNum(i + 1)}分`)
-  dateTimeList.secondList = Array.from({ length: 60 }, (_, i) => `${filterNum(i + 1)}秒`)
+  dateTimeList.hourList = Array.from({ length: 24 }, (_, i) => `${filterNum(i)}时`)
+  dateTimeList.minuteList = Array.from({ length: 60 }, (_, i) => `${filterNum(i)}分`)
+  dateTimeList.secondList = Array.from({ length: 60 }, (_, i) => `${filterNum(i)}秒`)
   dateTimeList.ampmList = ['上午', '下午']
   hasT.value = true
 }
 init()
 
 document.addEventListener('visibilitychange', e => {
-  console.log(e.target.visibilityState)
   if (e.target.visibilityState === 'visible') {
     init()
   } else if (e.target.visibilityState === 'hidden') {
@@ -231,20 +237,16 @@ document.addEventListener('visibilitychange', e => {
 
 .clock {
   position: absolute;
-  width: 0;
-  height: 0;
+  width: 20px;
+  height: 20px;
   top: 50%;
   left: 50%;
   display: flex;
   text-align: right;
-}
-.year.rotate_box {
-  left: -40px;
-  top: -40px;
+  transform: translate(-50%, -50%);
 }
 .year.rotate_box div {
   background: skyblue;
-  position: absolute;
   width: 80px;
   height: 80px;
   border-radius: 50%;
@@ -253,23 +255,26 @@ document.addEventListener('visibilitychange', e => {
 }
 .rotate_box {
   position: absolute;
-  left: -35px;
-  top: -10px;
-  transform-origin: 35px 10px;
-  margin: 0;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
 }
 .rotate_box div {
   position: absolute;
+  top: 50%;
+  left: 50%;
   width: 70px;
   height: 20px;
+  transform: translate(-50%, -50%);
 }
 
 .shade {
   position: absolute;
   background: skyblue;
-  width: 430px;
-  height: 28px;
-  left: 20px;
-  top: -14px;
+  width: 480px;
+  height: 30px;
+  top: 50%;
+  transform: translate(0, -50%);
 }
 </style>
