678ab0bacb3fbde0ee8170275f1a38d4af391ec2
YangH9 <49155616+YangH9@users.noreply.github.com>
2024-10-09 16:08:41 +0800

    [update]自动提交

---
+++ src/views/study/component-demo/richtext-umoeditor.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <Breadcrumb overlayShow />
+    <a-card v-calcHeight="{ height: 18, dom: '.ant-card-body' }" title="富文本-umoeditor" :hoverable="true">
+      umoeditor
+    </a-card>
+  </div>
+</template>
+
+<script setup lang="jsx">
+import Breadcrumb from '@/components/Breadcrumb.vue'
+import { ref } from 'vue'
+
+// https://editor.umodoc.com/
+</script>
+
+<style scoped lang="scss">
+.ant-card:deep(.ant-card-body) {
+  overflow-x: hidden;
+  overflow-y: auto;
+}
+</style>
