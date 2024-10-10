475196c4a902291f38972afbef9315536725dba2
YangH9 <49155616+YangH9@users.noreply.github.com>
2024-10-10 18:25:04 +0800

    [update]自动提交

---
+++ src/views/study/other/virtual-joystick.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <Breadcrumb overlayShow />
+    <a-card v-calcHeight="{ height: 21, dom: '.ant-card-body' }" title="虚拟摇杆" :hoverable="true">
+      <div>四向按钮</div>
+      <div>八向按钮</div>
+      <div>四向摇杆</div>
+      <div>八向摇杆</div>
+      <div>万向摇杆</div>
+    </a-card>
+  </div>
+</template>
+
+<script setup lang="jsx">
+import Breadcrumb from '@/components/Breadcrumb.vue'
+// 四向按钮
+// 八向按钮
+// 四向摇杆
+// 八向摇杆
+// 万向摇杆
+</script>
+
+<style scoped lang="scss"></style>
